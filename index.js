9041ac5d0a4970213814b548a04d7d0b73a67fb9
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-07-05 16:11:34 +0500

    Working

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+console.log(chalk.yellowBright.bold("\n \t Wellcome to the code with Sameer Anis' - Adventure Game\n"));
+//--------------------------Games Variable--------------------------------
+let enemies = ["Skeleton", "Zombie", "Warrior", "Assassin"];
+let maxEnemyHealth = 75;
+let enemyAttackDamageToHero = 25;
+//--------------------------------Player Variable ---------------------------
+let heroHealth = 100;
+let attackDamageToEnemy = 50;
+let numHealthPortion = 3;
+let healthPortionHealAmount = 30;
+let healthPortionDropChance = 50;
+//--------------------------While Loop  Condition--------------------
+let gameRunning = true;
+console.log("Wellcome to DeadZone");
+Game: while (gameRunning) {
+    let enemyHealth = Math.floor(Math.random() * maxEnemyHealth + 1);
+    let enemyIndex = Math.floor(Math.random() * enemies.length);
+    let enemy = enemies[enemyIndex];
+    console.log(`# ${enemy} has appeared #\n`);
+}
